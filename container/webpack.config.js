79dcbfab67bf7dd2e5c35bdf43b35ecfc1ab1b45
boseaditya1994 <boseaditya1994@gmail.com>
2026-06-23 21:43:00 +0530

    The Development Process

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -11,6 +11,7 @@ const config = {
       name: "container",
       remotes: {
         products: "products@http://localhost:8081/remoteEntry.js",
+        cart: 'cart@http://localhost:8082/remoteEntry.js'
       },
     }),
     new HtmlWebpackPlugin({
