494c344228711f0c16f13369fe19236dccf60347
boseaditya21 <AdityaBose@21>
2026-06-23 11:51:30 +0530

    The Basics of Microfrontends

---
+++ container/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+// Workaround: Delete the problematic internal property if a plugin injects it
+if (config.devServer && config.devServer._assetEmittingPreviousFiles) {
+  delete config.devServer._assetEmittingPreviousFiles;
+}
+
+module.exports = config;
---
+++ products/src/index.js
@@ -0,0 +1,11 @@
+import faker from "faker";
+
+let products = "";
+
+for (let i = 0; i < 5; i++) {
+  const name = faker.commerce.productName();
+  // Wrap the HTML in backticks to use string interpolation
+  products += `<div>${name}</div>\n`;
+}
+
+document.querySelector("#dev-products").innerHTML = products;
